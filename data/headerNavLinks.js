42e5c1311b32feafa4a6750d1db56a8d8dfe52f2
Antonio Sánchez <9774942+Nakerdev@users.noreply.github.com>
2023-09-12 13:54:19 +0000

    created library page

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,6 +1,7 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
+  { href: '/library', title: 'Biblioteca' },
   //{ href: '/projects', title: 'Projects' },
   { href: '/about', title: 'Sobre mí' },
 ]
---
+++ pages/library.js
@@ -0,0 +1,39 @@
+const readedBooks = [
+  'Diseño ágil con TDD - Carlos Blé',
+  'Extreme Programming Explained: Embrace Change, 2nd Edition',
+  'Clean Code - Robert C. Martin',
+  'Domain-Driven Design Distilled',
+  '4 Rules of simple design - Kent Beck',
+  'Thinking functionally',
+  'Clean architecture - Robert C. Martin',
+  'Agile Retrospectives',
+]
+
+export default function Projects() {
+  return (
+    <>
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Biblioteca
+          </h1>
+        </div>
+        <div className="container py-12">
+          <div className="-m-4 flex flex-wrap">
+            <h2 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+              Leidos
+            </h2>
+            {readedBooks.map((book, index) => (
+              <h3
+                key={index}
+                className="text-xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14"
+              >
+                {book}
+              </h3>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
