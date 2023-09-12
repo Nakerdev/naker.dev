335e78bae01835c70891ffb9204cc73e2d921154
Antonio Sánchez <9774942+Nakerdev@users.noreply.github.com>
2023-09-12 13:58:28 +0000

    change styles

--- pages/library.js
+++ pages/library.js
@@ -14,19 +14,19 @@ export default function Projects() {
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100">
             Biblioteca
           </h1>
         </div>
         <div className="container py-12">
           <div className="-m-4 flex flex-wrap">
-            <h2 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            <h2 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100">
               Leidos
             </h2>
             {readedBooks.map((book, index) => (
               <h3
                 key={index}
-                className="text-xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14"
+                className="text-xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100"
               >
                 {book}
               </h3>
